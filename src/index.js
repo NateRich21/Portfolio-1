23faa8c71939ea329db9a2d4a629a7a50b28aa66
Nathan Richardson <nathanjrichardson3@gmail.com>
2020-02-13 13:06:35 -0600

    Added MDB to the project and am currently plotting the layout.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App/App.js';
+import {Link, Route, BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './Components/App/App.js';
+import CustomNavBar from './Components/CustomerNavBar/CustomNavBar.js';
+import Home from './Components/Home/Home.js'
+import Projects from './Components/Projects/Projects'
+import About from './Components/About/About.js'
+
+
+class Index extends React.Component {
+
+    render() {
+        return(
+        
+            <Router>
+                <CustomNavBar />
+
+                <Route 
+                    path="/home"
+                    exact
+                    component={Home} />
+                <Route
+                    path="/about"
+                    exact
+                    component={About} />
+                <Route 
+                    path="/projects"
+                    exact
+                    component={Projects}/>
+            </Router>
+        )
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
