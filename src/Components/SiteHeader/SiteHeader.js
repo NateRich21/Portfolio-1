1094034c0be518edf152378e0081cf17593b6cb8
Nathan Richardson <nathanjrichardson3@gmail.com>
2020-02-13 19:01:55 -0600

    added site header component

---
+++ src/Components/SiteHeader/SiteHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SiteHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      path: ''
+    }
+  }
+
+  render() {
+    return(
+      
+    )
+  }
+}
