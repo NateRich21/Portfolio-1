e4b4a013d142b6af31e639d9a084eeeb59cf1c29
Nathan Richardson <nathanjrichardson3@gmail.com>
2020-02-14 21:42:06 -0600

    A few cosmetic changes to the About page

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -16,21 +16,25 @@ class About extends React.Component {
             
             <div className='about-page-container'>
                 <div className='about-page'>
-                    <h2 className='content-lg'>Who I am</h2>
+                    <h2 className='content-lg-top'>Who I am</h2>
                     <p className='content-sm'> I'm Nate Richardson, and I'm passionate about learning and creating</p>
                     <p className='content-sm'>By day, I work in IT support. In the evenings, I work to become the best developer I can be.</p>
+                    <hr />
                     <h2 className='content-lg'>Why I want to develop</h2>
                     <p className="content-sm">As someone with a great deal of passion around creativity, I love that programming allows me to take nearly any idea I have and make it a reality.</p>
                     <p className='content-sm'>In addition to what it allows me to accomplish, I enjoy the process as a whole. I love problem solving, so of course I love to code. </p>
+                    <hr />
                     <h2 className='content-lg'>What I know</h2>
                     <p className='content-sm'>My primary focus has been web development. Although I have also started building native VR apps for android devices.</p>
                     <p className='content-sm'>Feel free to click below and get a feel for the technologies I have a experience with.</p>
+                    <hr />
                     <div className='button-row'>
                         <Link to='/tech'>
                             <MDBBtn color='info'>
                                 Technologies
                             </MDBBtn>
                         </Link>
+                        <hr />
                     </div>
                 </div>
             </div>
