c61ff48c9d4588614c413452b82796f79766bbcc
Nathan Richardson <nathanjrichardson3@gmail.com>
2020-02-14 20:24:11 -0600

    site is now hosted on firebase, need to get a custom domain so I can work out the smtp stuff. Also, I'm going to delete build and re-build then re-deploy to fix a css issue. THIS IS THE COMMIT to come back to if that breaks something.

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -16,10 +16,10 @@ class About extends React.Component {
             
             <div className='about-page-container'>
                 <div className='about-page'>
-                    <h2>Who I am</h2>
+                    <h2 className='content-lg'>Who I am</h2>
                     <p className='content-sm'> I'm Nate Richardson, and I'm passionate about learning and creating</p>
                     <p className='content-sm'>By day, I work in IT support. In the evenings, I work to become the best developer I can be.</p>
-                    <h2>Why I want to develop</h2>
+                    <h2 className='content-lg'>Why I want to develop</h2>
                     <p className="content-sm">As someone with a great deal of passion around creativity, I love that programming allows me to take nearly any idea I have and make it a reality.</p>
                     <p className='content-sm'>In addition to what it allows me to accomplish, I enjoy the process as a whole. I love problem solving, so of course I love to code. </p>
                     <h2 className='content-lg'>What I know</h2>
